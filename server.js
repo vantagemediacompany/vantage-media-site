def9139bf0c79c891f5e9ddefa4ed08be18da7d1
Vizena2005 <vizena2005@gmail.com>
2018-10-03 11:58:57 -0500

    Everything is working for contact.handlebars except for the links from the other pages

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-var express = require("express"),
-    app = express(),
-    path = require("path");
+const express = require("express");
 
+const app = express();
+
+// Static
 public = require("path").join(__dirname, "/dist");
 app.use(express.static(public));
 
+// Sets the port
 var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
-/*such changes*/
