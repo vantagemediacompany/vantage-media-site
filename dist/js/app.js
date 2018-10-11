913cc4e072820afe16e707c1ce9d9de339dd0599
Vizena2005 <vizena2005@gmail.com>
2018-10-11 14:01:15 -0500

    Barba.js loaded in with basic fade in/fade out transition

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,49 +1,29 @@
 $(document).ready(function() {
-  // const t1 = new TimelineMax({ paused: true });
-
-  // //   Menu animations
-  // t1.to(".btn-line-one", 0.8, {
-  //   y: 6,
-  //   rotation: 45,
-  //   ease: Expo.easeInOut
-  // });
-
-  // t1.to(".btn-line-two", 0.8, {
-  //   y: -18,
-  //   rotation: -45,
-  //   ease: Expo.easeInOut,
-  //   delay: -0.8
-  // });
-
-  // t1.to(".menu-span", 0.8, {
-  //   opacity: 0,
-  //   ease: Expo.easeInOut,
-  //   delay: -1
-  // });
-
-  // t1.to(".menu", 1, {
-  //   top: "0%",
-  //   ease: Expo.easeInOut,
-  //   delay: -1
-  // });
-
-  // t1.staggerFrom(
-  //   ".menu ul li",
-  //   1,
-  //   { x: -200, opacity: 0, ease: Expo.easeOut },
-  //   0.3
-  // );
-
-  // t1.reverse();
-  // $(document).on("click", ".menu-btn", function() {
-  //   t1.reversed(!t1.reversed());
-  //   setTimeout(() => {
-  //     window.scrollTo(0, 0);
-  //   }, 1200);
-  // });
-  // $(document).on("click", "a", function() {
-  //   t1.reversed(!t1.reversed());
-  // });
+  // Barba transition
+  var transEffect = Barba.BaseTransition.extend({
+    start: function() {
+      this.newContainerLoading.then(val =>
+        this.fadeInNewcontent($(this.newContainer))
+      );
+    },
+    fadeInNewcontent: function(nc) {
+      nc.hide();
+      var _this = this;
+      $(this.oldContainer)
+        .fadeOut(1000)
+        .promise()
+        .done(() => {
+          nc.css("visibility", "visible");
+          nc.fadeIn(1000, function() {
+            _this.done();
+          });
+        });
+    }
+  });
+  Barba.Pjax.getTransition = function() {
+    return transEffect;
+  };
+  Barba.Pjax.start();
 
   // Menu toggle-button
   $(".menu-btn").on("click", function() {
@@ -62,55 +42,46 @@ $(document).ready(function() {
   //   Scroll reveal animations
   window.sr = ScrollReveal();
   sr.reveal(".menu-nav li", {
-    mobile: false,
     duration: 2000,
     origin: "right",
     distance: "300px"
   });
   sr.reveal(".menu-branding", {
-    mobile: false,
     duration: 2000,
     origin: "left",
     distance: "300px"
   });
   sr.reveal(".border", {
-    mobile: false,
     duration: 2000,
     origin: "bottom",
     distance: "50px"
   });
   sr.reveal(".icons", {
-    mobile: false,
     duration: 250,
     origin: "bottom",
     distance: "300px"
   });
   sr.reveal(".services", {
-    mobile: false,
     duration: 1000,
     origin: "bottom",
     distance: "300px"
   });
   sr.reveal(".bottomFadeIn", {
-    mobile: false,
     duration: 1000,
     origin: "bottom",
     distance: "50px"
   });
   sr.reveal(".topFadeIn", {
-    mobile: false,
     duration: 1000,
     origin: "top",
     distance: "50px"
   });
   sr.reveal(".leftFadeIn", {
-    mobile: false,
     duration: 1000,
     origin: "left",
     distance: "50px"
   });
   sr.reveal(".rightFadeIn", {
-    mobile: false,
     duration: 1000,
     origin: "right",
     distance: "50px"
